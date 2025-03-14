a7e5f5b3b40bc29fae430fec121745ce8010f8cc
salome <salomenjinda0@gmail.com>
2025-03-14 10:28:59 +0100

    upadted controller

--- app/Http/Controllers/SituationController.php
+++ app/Http/Controllers/SituationController.php
@@ -14,14 +14,13 @@ use Illuminate\Support\Facades\Validator;
  *
  * Endpoints related to user situations management.
  */
-
 class SituationController extends Controller
 {
-        /**
+    /**
      * Display all situations under a specific situation.
      */
 
-/**
+    /**
      * Get user situations details.
      *
      * Retrieve details of a user situations by their ID.
@@ -29,40 +28,39 @@ class SituationController extends Controller
      * @urlParam id int required The ID of the user situations. Example: 1
      *
      * @response 200 {
-    *"success": true,
-    *"data": [
-    *    {
-    *        "id": 2,
-    *        "title": "Gate man was arrest",
-    *        "description": "He's am going through a lot",
-    *        "image": "/storage/images/sHNcE5cszEFpEYu770KvndsYVtYqSV8biQDgvB0Y.png",
-    *        "created_at": "2025-01-21T11:09:12.000000Z"
-    *    },
-    *    {
-    *        "id": 3,
-    *        "title": "Hello mimi lena",
-    *        "description": "Call me when you can",
-    *        "image": "/storage/images/YC2W1ITiioam6AfvVp0JScuZBXsunRJnLzeQTp4X.png",
-    *        "created_at": "2025-01-21T11:25:36.000000Z"
-    *    },
-    *    {
-    *        "id": 4,
-    *        "title": "Does not go to school",
-    *        "description": "yes i don't",
-    *        "image": null,
-    *        "created_at": "2025-01-21T11:26:29.000000Z"
-    *    }
-    *]
-*}
+     *"success": true,
+     *"data": [
+     *    {
+     *        "id": 2,
+     *        "title": "Gate man was arrest",
+     *        "description": "He's am going through a lot",
+     *        "image": "/storage/images/sHNcE5cszEFpEYu770KvndsYVtYqSV8biQDgvB0Y.png",
+     *        "created_at": "2025-01-21T11:09:12.000000Z"
+     *    },
+     *    {
+     *        "id": 3,
+     *        "title": "Hello mimi lena",
+     *        "description": "Call me when you can",
+     *        "image": "/storage/images/YC2W1ITiioam6AfvVp0JScuZBXsunRJnLzeQTp4X.png",
+     *        "created_at": "2025-01-21T11:25:36.000000Z"
+     *    },
+     *    {
+     *        "id": 4,
+     *        "title": "Does not go to school",
+     *        "description": "yes i don't",
+     *        "image": null,
+     *        "created_at": "2025-01-21T11:26:29.000000Z"
+     *    }
+     *]
+     *}
      * @response 404 {
      *"success": false,
      *"message": "No Situation available"
      *}
      */
-
     public function index()
     {
-        $situation = Situation::get();
+        $situation = Situation::with(['user'])->get();
         if ($situation->count() > 0) {
             return SituationResource::collection($situation);
         } else {
@@ -70,7 +68,7 @@ class SituationController extends Controller
         }
     }
 
-        /**
+    /**
      * Store a new situation in the database.
      */
 
@@ -82,22 +80,19 @@ class SituationController extends Controller
      * @urlParam id int required The ID of the user suggestions. Example: 1
      *
      * @response 200 {
-    *"message": "Situation created successfully",
-    *"data": {
-    *    "id": 5,
-    *    "title": "Had an arguement with the police",
-    *    "description": "yes i dis",
-    *    "image": null,
-    *    "created_at": "2025-01-24T13:31:21.000000Z"
-    *}
-*}
+     *"message": "Situation created successfully",
+     *"data": {
+     *    "id": 5,
+     *    "title": "Had an arguement with the police",
+     *    "description": "yes i dis",
+     *    "image": null,
+     *    "created_at": "2025-01-24T13:31:21.000000Z"
+     *}
+     *}
      * @response 401 {
      *"message": "Unauthenticated."
      *}
      */
-
-
-     
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -107,14 +102,13 @@ class SituationController extends Controller
             'is_sensitive' => 'boolean',
         ]);
 
-        if($validator->fails())
-         {
+        if ($validator->fails()) {
             return response()->json([
-              'message'=>'All fields are mandatory',
-              'error'=> $validator->messages(),
-             
+                'message' => 'All fields are mandatory',
+                'error' => $validator->messages(),
+
             ], 422);
-         }
+        }
 
         $situation = Situation::create([
             'title' => $request->title,
@@ -124,17 +118,17 @@ class SituationController extends Controller
         ]);
 
         if ($request->hasFile('image')) {
-                try {
-                    $path = Storage::url($request->file('image')->store('images', 'public'));
-                    $situation->image = $path;
-                    $situation->save();
-                } catch (\Exception $e) {
-                    logger()->error('File upload failed', [
-                        'error' => $e->getMessage(),
-                        'trace' => $e->getTraceAsString(),
-                    ]);
-                }
+            try {
+                $path = Storage::url($request->file('image')->store('images', 'public'));
+                $situation->image = $path;
+                $situation->save();
+            } catch (\Exception $e) {
+                logger()->error('File upload failed', [
+                    'error' => $e->getMessage(),
+                    'trace' => $e->getTraceAsString(),
+                ]);
             }
+        }
 
         return response()->json([
             'message' => 'Situation created successfully',
@@ -143,11 +137,11 @@ class SituationController extends Controller
 
     }
 
-/**
+    /**
      * Display a specific situation.
      */
 
-     /**
+    /**
      * Get user situation details.
      *
      * Retrieve details of a situation suggestions by their ID.
@@ -157,21 +151,20 @@ class SituationController extends Controller
      * @response 200 {
      *"success": true,
      *"data": {
-    *"data": {
-    *    "id": 5,
-    *    "title": "Had an arguement with the police",
-    *    "description": "yes i dis",
-    *    "image": null,
-    *    "created_at": "2025-01-24T13:31:21.000000Z"
-    *}
-*}
+     *"data": {
+     *    "id": 5,
+     *    "title": "Had an arguement with the police",
+     *    "description": "yes i dis",
+     *    "image": null,
+     *    "created_at": "2025-01-24T13:31:21.000000Z"
+     *}
+     *}
      * @response 404 {
      *"success": false,
      *"message": "Situation not found"
      *}
      */
-
-    public function show(int $id) 
+    public function show(int $id)
     {
         $situation = Situation::find($id);
 
@@ -181,7 +174,7 @@ class SituationController extends Controller
                 'message' => 'Situation not found',
             ], 404);
         }
-       
+
         return new SituationResource($situation);
     }
 
@@ -189,7 +182,7 @@ class SituationController extends Controller
      * Update a situation in the database.
      */
 
-     /**
+    /**
      * Put user situation details.
      *
      * Retrieve details of a user situation by their ID.
@@ -224,9 +217,8 @@ class SituationController extends Controller
      *"message": "Suggestion not found"
      *}
      */
-
     public function update(Request $request, int $id)
-     {
+    {
         $situation = Situation::find($id);
         logger($situation);
         $validator = Validator::make($request->all(), [
@@ -234,17 +226,16 @@ class SituationController extends Controller
             'description' => 'sometimes',
             'image' => 'nullable|file|max:51200',
             'is_sensitive' => 'boolean',
-            
+
         ]);
 
-        if($validator->fails())
-         {
+        if ($validator->fails()) {
             return response()->json([
-              'message'=>'All fields are mandatory',
-              'error'=> $validator->messages(),
-             
+                'message' => 'All fields are mandatory',
+                'error' => $validator->messages(),
+
             ], 422);
-         }
+        }
 
         $situation->update([
             'title' => $request->title ?? $situation->title,
@@ -253,69 +244,66 @@ class SituationController extends Controller
         ]);
 
         if ($request->hasFile('image')) {
-                try {
-                    $path = Storage::url($request->file('image')->store('images', 'public'));
-                    $situation->image = $path;
-                    $situation->save();
-                } catch (\Exception $e) {
-                    logger()->error('File upload failed', [
-                        'error' => $e->getMessage(),
-                        'trace' => $e->getTraceAsString(),
-                    ]);
-                }
+            try {
+                $path = Storage::url($request->file('image')->store('images', 'public'));
+                $situation->image = $path;
+                $situation->save();
+            } catch (\Exception $e) {
+                logger()->error('File upload failed', [
+                    'error' => $e->getMessage(),
+                    'trace' => $e->getTraceAsString(),
+                ]);
             }
+        }
 
         return response()->json([
             'message' => 'Situation updated successfully',
             'data' => new SituationResource($situation),
         ], 200);
 
-     }
+    }
 
     public function destroy(int $id)
-     {
+    {
         $situation = Situation::find($id);
         logger($situation);
         $situation->delete();
+
         return response()->json([
             'message' => 'Situation deleted successfully',
         ], 200);
-     }
+    }
 
-
-
-     public function search()
+    public function search()
     {
         try {
 
             $query = Situation::query();
 
             if (request()->has('search')) {
-            $search = request()->query('search');
+                $search = request()->query('search');
 
-            $query->where(function ($q) use ($search) {
-                $q->where('title', 'LIKE', "%$search%")
-                    ->orWhere('description', 'LIKE', "%$search%");
-            });
-        }
+                $query->where(function ($q) use ($search) {
+                    $q->where('title', 'LIKE', "%$search%")
+                        ->orWhere('description', 'LIKE', "%$search%");
+                });
+            }
 
             $situations = $query->get();
 
             return response()->json([
                 'success' => true,
                 'data' => SituationResource::collection($situations),
-               
+
             ], 200);
-            
 
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'An error occurred while searching',
-                'error' => $e->getMessage()
+                'error' => $e->getMessage(),
             ], 500);
         }
-        
+
     }
-    
 }
--- app/Models/Situation.php
+++ app/Models/Situation.php
@@ -22,6 +22,6 @@ class Situation extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
